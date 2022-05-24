1d7a492a78e671e7c688c57d3d3aa451f6821428
cwj980119 <cwj980119@g.hongik.ac.kr>
2022-05-25 00:03:28 +0900

    Add input_list_ohe.json

--- input_onehot.py
+++ input_onehot.py
@@ -1,5 +1,4 @@
 import json
-import numpy
 import random
 
 with open('./players/input_list.json', 'r') as f: #mat 불러오기
@@ -8,10 +7,11 @@ with open('./champion.json', 'r') as f: #mat 불러오기
     champ_list = json.load(f)
 
 input_list_ohe = []
-
+random.shuffle(input_list)
 for i in input_list:
     rand = [0,1,2,3,4]
-    rand = random.shuffle(rand)
+    random.shuffle(rand)
+    print(i)
     shuffle_list = []
 
     for j in rand:
@@ -20,23 +20,22 @@ for i in input_list:
 
 
     for j in range(5):
-        champ = np.zeros(159)
-        for k in len(champ_list):
+        champ = [0 for t in range(159)]
+        for k in range(len(champ_list)):
             if shuffle_list[j*2] == int(champ_list[k]["key"]):
                 champ[k] = 1
                 break
         shuffle_list[j*2] = champ
 
     for j in range(5):
-        lane = np.zeros(6)
+        lane = [0 for t in range(6)]
         lane[shuffle_list[j*2 + 1]] = 1
         shuffle_list[j*2 + 1] = lane
 
     shuffle_list.append(i[-1])
     input_list_ohe.append(shuffle_list)
-
 with open('./players/input_list_ohe.json', 'w') as f:    #match 저장
-    json.dump(input_list_ohe, f, ensure_ascii=False, indent=4)
+    json.dump(input_list_ohe, f, ensure_ascii=False)
 
 
 
